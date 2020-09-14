a1a95548556017efc90cae889a78c7ce6364691c
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-09-14 09:16:09 +0000

    Configure Destination Service for Customer (Quarkus)
    You can now have the customer service target either RecommendationService or PreferenceService based on the ConfigProperty destination.service
    
    Can also drive by environment variable DESTINATION_SERVICE

--- customer/java/quarkus/src/main/java/com/redhat/developer/demos/customer/rest/CustomerResource.java
+++ customer/java/quarkus/src/main/java/com/redhat/developer/demos/customer/rest/CustomerResource.java
@@ -1,10 +1,12 @@
 package com.redhat.developer.demos.customer.rest;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.lang.model.util.ElementScanner6;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,6 +18,13 @@ public class CustomerResource {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @ConfigProperty(name = "destination.service", defaultValue="RecommendationService")
+    String targetService;
+
+    @Inject
+    @RestClient
+    RecommendationService recommendationService;
+
     @Inject
     @RestClient
     PreferenceService preferenceService;
@@ -23,12 +32,30 @@ public class CustomerResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public Response getCustomer() {
+
         try {
-            String response = preferenceService.getPreference().trim();
+            String response;
+
+            if (targetService.equals("RecommendationService")) {
+                response = recommendationService.getRecommendation().trim();
+            }
+            else if (targetService.equals("PreferenceService"))
+            {
+                response = preferenceService.getPreference().trim();
+            }
+            else
+            {
+                return Response
+                    .status(Response.Status.NOT_IMPLEMENTED)
+                    .entity(String.format(RESPONSE_STRING_FORMAT, 
+                            String.format("Service %s is unknown", targetService)))
+                    .build();
+            }
+
             return Response.ok(String.format(RESPONSE_STRING_FORMAT, response)).build();
-        } catch (WebApplicationException ex) {
+         } catch (WebApplicationException ex) {
             Response response = ex.getResponse();
-            logger.warn("Non HTTP 20x trying to get the response from preference service: " + response.getStatus());
+            logger.warn(String.format("Non HTTP 20x trying to get the response from %s service: %s", targetService, response.getStatus()));
             return Response
                     .status(Response.Status.SERVICE_UNAVAILABLE)
                     .entity(String.format(RESPONSE_STRING_FORMAT,
@@ -36,7 +63,7 @@ public class CustomerResource {
                     )
                     .build();
         } catch (ProcessingException ex) {
-            logger.warn("Exception trying to get the response from preference service.", ex);
+            logger.warn(String.format("Exception trying to get the response from %s service.", targetService), ex);
             return Response
                     .status(Response.Status.SERVICE_UNAVAILABLE)
                     .entity(String.format(RESPONSE_STRING_FORMAT, ex.getCause().getClass().getSimpleName() + ": " + ex.getCause().getMessage()))
